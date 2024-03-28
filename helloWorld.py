34dc27f2ea2c4cb011bb4acc1ffab48a65821efa
Enam <enamkobby@gmail.com>
2024-03-28 19:54:38 +0000

    Added comments to improve code readability

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,5 @@
-user_input = input("Enter your message: ")
-print(user_input)
+# Ask the user for their name
+name = input("Please enter your name: ")
+
+# Print a personalized welcome message
+print(f"Welcome, {name}!")
